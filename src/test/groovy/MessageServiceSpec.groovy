7df0efd723390cf22f67f71e141fcb0acb297c29
maxl <max.leow@experian.com>
2019-07-31 18:51:16 +0800

    update for new structure

--- src/test/groovy/MessageServiceSpec.groovy
+++ src/test/groovy/MessageServiceSpec.groovy
@@ -6,8 +6,6 @@ class MessageServiceSpec extends Specification {
     @CaseId(22615457)
     def "Get message"() {
 
-        throw new Exception("opps")
-
         expect: true
     }
 
--- src/test/java/ComponentATest.java
+++ src/test/java/ComponentATest.java
@@ -1,6 +1,8 @@
 import com.achelois.helical.annotations.CaseId;
+import org.junit.Test;
 
 public class ComponentATest {
+    @Test
     public void testMethod1(){
 
     }
