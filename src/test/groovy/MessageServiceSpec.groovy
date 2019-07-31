b0383909535ff981357bb033dfe39562c945a40a
maxl <max.leow@experian.com>
2019-07-31 18:44:16 +0800

    update for new structure

--- src/test/groovy/MessageServiceSpec.groovy
+++ src/test/groovy/MessageServiceSpec.groovy
@@ -1,13 +1,14 @@
 import com.achelois.helical.annotations.CaseId
-import com.achelois.helical.annotations.RunId
 import spock.lang.Specification
 
-@RunId(11)
 class MessageServiceSpec extends Specification {
 
-    @CaseId(6)
-    def 'Get message'() {
-        expect: 'Should return the correct message'
+    @CaseId(22615457)
+    def "Get message"() {
+
+        throw new Exception("opps")
+
+        expect: true
     }
 
 }
--- src/test/java/ComponentATest.java
+++ src/test/java/ComponentATest.java
@@ -1,11 +1,7 @@
 import com.achelois.helical.annotations.CaseId;
-import com.achelois.helical.annotations.RunId;
-import org.junit.Test;
 
-@RunId(23972)
 public class ComponentATest {
-    @Test @CaseId(22615457)
     public void testMethod1(){
-        System.out.println("testing method1");
+
     }
 }
